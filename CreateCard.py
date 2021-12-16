72b32b823895663dbf3d9e79a117bc2cec332b48
Mika Nokka <mika.nokka1@gmail.com>
2021-12-16 18:26:06 +0200

    Added parameters, now as a command

--- CreateCard.py
+++ CreateCard.py
@@ -7,20 +7,67 @@
 import requests
 import os 
 import sys
+import sys, logging
+import argparse
+
+__version__ = u"0.1"
+
+logging.basicConfig(level=logging.DEBUG) # IF calling from Groovy, this must be set logging level DEBUG in Groovy side order these to be written out
+
 
 
 def main(argv):
 
+
+    parser = argparse.ArgumentParser(description="Create Trello card",
+    
+    
+    epilog="""
+    
+    Requires env variables for Trello authentication to exists:
+    
+    TOKEN (set in trello account settings)
+    
+    KEY (https://trello.com/app-key)
+    
+    LISTID (the id for your list, add .json end of trello  url and find list: id:)
+    
+    
+    EXAMPLE:
+    
+    CreateCard.py  -n CARDNAME"""
+
+    
+    )
+
+
+
+    parser.add_argument('-v', help='Show version&author and exit', action='version',version="Version:{0}   mika.nokka1@gmail.com".format(__version__) )  
+    parser.add_argument("-n",help='<Name of the new Trello Card>',metavar="cardname")
+
+    args = parser.parse_args()
+       
+    CARDNAME = args.n or ''
+  
+
+    #logging.info("CARDNAME:{0}".format(CARDNAME))
+    
+    # quick old-school way to check needed parameters
+    if (CARDNAME==''):
+        logging.error("\n---> MISSING ARGUMENTS!!\n ")
+        parser.print_help()
+        sys.exit(2)
+
+
     #Get env variables from file (do not store to Github)
-    token=GetEnvVar("TOKEN") # set in trello account settin
-    key=GetEnvVar("KEY") #h ttps://trello.com/app-key
+    token=GetEnvVar("TOKEN") # set in trello account settings
+    key=GetEnvVar("KEY") #https://trello.com/app-key
     list_id=GetEnvVar("LISTID") # the id for your list, add .json end of trello  url and find list: id:
  
-    
-    card_name="kissa-testi"
+
 
     url = f"https://api.trello.com/1/cards"
-    querystring = {"name": card_name, "idList": list_id, "key": key, "token": token} # full restapi: https://developer.atlassian.com/cloud/trello/rest/api-group-cards/#api-cards-post
+    querystring = {"name": CARDNAME, "idList": list_id, "key": key, "token": token} # full restapi: https://developer.atlassian.com/cloud/trello/rest/api-group-cards/#api-cards-post
     response = requests.request("POST", url, params=querystring)
     print ("response: {0}".format(response))
     #print ("Got full json: {0}".format(response.json()))
@@ -37,6 +84,7 @@ def GetEnvVar(variable):
         return str(value)
     else:
         print("Error.Env variable {0} NOT found.EXITING!!".format(variable))
+        parser.print_help()
         sys.exit(5)
    
     
